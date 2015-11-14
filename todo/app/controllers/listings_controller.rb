39e4064260a44a907741efc0e7866e3086883b01
Michael Aldaba <maldaba3@gmail.com>
2015-11-14 18:40:44 -0500

    Add new todo from UI

--- todo/app/controllers/listings_controller.rb
+++ todo/app/controllers/listings_controller.rb
@@ -4,4 +4,21 @@ class ListingsController < ApplicationController
   	@listings = Listing.all
  	end
  	
+ 	def new
+ 		@listing = Listing.new
+ 	end
+
+ 	def create
+ 		@listing = Listing.new(listing_params)
+ 		if @listing.save
+ 			redirect_to listings_path
+ 		else
+ 			render 'new'
+ 		end
+	end
+
+	private
+		def listing_params
+			params.require(:listing).permit(:description)
+		end
 end
